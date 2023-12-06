771a2b32255a74f9b1e00c091c584349d4707865
tete-cordobes <45921417+tete-cordobes@users.noreply.github.com>
2023-12-06 12:24:09 +0100

    Add files via upload

---
+++ hola.py
@@ -0,0 +1,10 @@
+# Primero, instala Streamlit si aún no lo has hecho:
+# pip install streamlit
+
+# Luego, crea un script de Python (por ejemplo, hola_mundo.py) y escribe lo siguiente:
+
+import streamlit as st
+
+st.title('Hola Mundo con Streamlit')
+
+st.write('¡Bienvenido a tu primera aplicación con Streamlit!')
